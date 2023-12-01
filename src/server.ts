f50fb3a326550d6980d9ea40076e318d382d1e02
Edgar Chaver <edieveg316@gmail.com>
2023-12-01 16:50:47 -0600

    update modules

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import morgan from 'morgan';
-import path from 'path';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import path from "path";
 
-import connectDB from './config/db';
-import { colorTheme } from './types/colorTheme'; 
+import connectDB from "./config/db";
+import { colorTheme } from "./types/colorTheme";
 
 dotenv.config();
 
@@ -13,11 +13,12 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
 }
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //CORS
 app.use(
@@ -27,21 +28,25 @@ app.use(
 	})
 );
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 	});
 } else {
-	app.get('/', (req, res) => {
-		res.send('API is running');
+	app.get("/", (req, res) => {
+		res.send("API is running");
 	});
 }
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(colorTheme.secondary.bold(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+	console.log(
+		colorTheme.secondary.bold(
+			`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+		)
+	);
 });
