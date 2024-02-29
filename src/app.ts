a51c26be45df50e0c36fb57443ef9ec3e4f7d87c
Edgar Chaver <edieveg316@gmail.com>
2024-02-29 17:22:24 -0600

    Update App file

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,8 @@ import path from "path";
 import connectDB from "./config/db";
 import { errorHandler, notFound } from "./middleware/errorHandler";
 import userRoutes from "./routes/userRoutes";
-import businessRoutes from "./routes/businessRoutes";
-import recipesRoutes from "./routes/recipesRoutes";
-import marketRoutes from "./routes/marketsRoutes";
-import restaurantRoutes from "./routes/restaurantRoutes";
-import doctorRoutes from "./routes/doctorsRoutes";
-import profession from "./routes/professionRoutes";
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
@@ -27,39 +20,22 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
-
-//CORS
 app.use(
-	cors({
-		credentials: true,
-		origin: process.env.FRONTEND_URL,
+	cors((req, callback) => {
+		const corsOptions = {
+			credentials: true,
+			origin: process.env.FRONTEND_URL,
+		};
+		callback(null, corsOptions);
 	})
 );
-
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-	});
-} else {
-	app.get("/api/v1", (req, res) => {
-		res.send("API is running");
-	});
-}
-
 // Routes
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/businesses", businessRoutes);
-app.use("/api/v1/recipes", recipesRoutes);
-app.use("/api/v1/markets", marketRoutes);
-app.use("/api/v1/restaurants", restaurantRoutes);
-app.use("/api/v1/doctors", doctorRoutes);
-app.use("/api/v1/professions", profession);
+app.use("/api/v1", userRoutes);
 
+// Error handling
 app.use(notFound);
 app.use(errorHandler);
 
