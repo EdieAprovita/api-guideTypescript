afa6aada96a2a889229bcde861efee69f788b1e1
Edgar  Chavero <53484526+EdieAprovita@users.noreply.github.com>
2024-03-01 08:33:51 -0600

    Fixing Routes

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,14 @@ import path from "path";
 
 import connectDB from "./config/db";
 import { errorHandler, notFound } from "./middleware/errorHandler";
+
 import userRoutes from "./routes/userRoutes";
+import businessRoutes from "./routes/businessRoutes";
+import recipesRoutes from "./routes/recipesRoutes";
+import marketsRoutes from "./routes/marketsRoutes";
+import restaurantRoutes from "./routes/restaurantRoutes";
+import doctorsRoutes from "./routes/doctorsRoutes";
+import professionRoutes from "./routes/professionRoutes";
 
 dotenv.config();
 connectDB();
@@ -32,8 +39,26 @@ app.use(
 );
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "client/build")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
+} else {
+	app.get("/api/v1", (req, res) => {
+		res.send("API is running");
+	});
+}
+
 // Routes
-app.use("/api/v1", userRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/businesses", businessRoutes);
+app.use("/api/v1/recipes", recipesRoutes);
+app.use("/api/v1/markets", marketsRoutes);
+app.use("/api/v1/restaurants", restaurantRoutes);
+app.use("/api/v1/doctors", doctorsRoutes);
+app.use("/api/v1/professions", professionRoutes);
 
 // Error handling
 app.use(notFound);
