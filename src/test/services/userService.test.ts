6ac9647c706e22b52ce1f80934a205573587ba11
Edgar  Chavero <53484526+EdieAprovita@users.noreply.github.com>
2025-06-06 10:18:58 -0600

    Handle missing user in update service

---
+++ src/test/services/userService.test.ts
@@ -0,0 +1,17 @@
+import UserService from "../../services/UserService";
+import { HttpError, HttpStatusCode } from "../../types/Errors";
+import { IUser } from "../../models/User";
+
+describe("UserService updateUserById", () => {
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it("should throw NotFound error when user does not exist", async () => {
+        jest.spyOn(UserService, "findUserById").mockResolvedValue(null as any);
+
+        await expect(UserService.updateUserById("1", {})).rejects.toThrow(
+            new HttpError(HttpStatusCode.NOT_FOUND, "Internal Server Error")
+        );
+    });
+});
