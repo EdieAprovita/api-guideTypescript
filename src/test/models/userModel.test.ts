654e116c4edebef56865fef3741e55b50f6a3d9a
Edgar  Chavero <53484526+EdieAprovita@users.noreply.github.com>
2025-06-10 18:36:33 -0600

    test: avoid hardcoded password

--- src/test/models/userModel.test.ts
+++ src/test/models/userModel.test.ts
@@ -1,6 +1,8 @@
 import { User } from "../../models/User";
+import { faker } from "@faker-js/faker";
 
-const DUMMY_PASSWORD = "testPass123!";
+// Generate a throwaway password for testing instead of using a hard-coded one
+const DUMMY_PASSWORD = faker.internet.password(12);
 
 describe("User model email validation", () => {
     it("accepts a valid email", () => {
